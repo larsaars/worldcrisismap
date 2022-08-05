99fe0db4967120749f45e1faa3d7d82cbb884baa
larsaars <lurzapps@gmail.com>
2022-08-05 20:14:33 +0200

    scrape reliefweb disaster api

--- crisis_database/past_crisis_collector.py
+++ crisis_database/past_crisis_collector.py
@@ -1,5 +1,85 @@
 #!/usr/bin/env python3
 
 """
-create
+Creates init_tables.sql (postgressql) file from scraping
+the ReliefWeb Disasters API.
+
+Meant to init the database, the server will then proceed to update the
+database every N hours.
+
+
+The reason to do this is, that it is unpracital (for both us and ReliefWeb)
+to use the API every time the timeline is used.
 """
+
+import requests
+import os
+
+
+# delete old file
+os.system('rm ./init_tables.sql')
+
+
+# PARAMS
+jumpsize = 100  # scraping jumpsize
+
+
+# open sql file in append mode
+file = open('./init_tables.sql', 'a')
+
+# init sql file 
+file.write('''-- clear complete database
+DROP TABLE disasters;
+
+-- create table
+
+CREATE TABLE IF NOT EXISTS disasters(
+    id INTEGER PRIMARY KEY,
+    date TIMESTAMP,
+    status VARCHAR(7),
+    country_name VARCHAR(50),
+    lat DECIMAL(10, 7),
+    lon DECIMAL(10, 7),
+    type VARCHAR(50),
+    url VARCHAR(100),
+    title VARCHAR(250),
+    overview_html VARCHAR(25000)
+)
+
+-- insert values into table
+''')
+
+# loop through
+current_offset, total_count = 0, 1
+
+while current_offset < total_count:
+    print(f'current offset: {current_offset}')
+
+    res = requests.get(f'https://api.reliefweb.int/v1/disasters?appname=crisis_collector&profile=full&preset=latest&slim=1&limit={jumpsize}&offset={current_offset}').json()
+
+    appendage = []  # what will be appended to file (insert into)
+
+
+    for item in res['data']:
+        try:
+            fd = item['fields']
+            country = fd['country'][0]
+            overview_html = fd['profile']['overview-html'].replace('\n', '<br>') if 'profile' in fd else '' 
+
+            appendage.append(f"INSERT INTO disasters(id, date, status, country_name, lat, lon, type, url, title, overview_html) VALUES ({item['id']}, '{fd['date']['event']}', '{fd['status']}', '{country['name']}', {country['location']['lat']}, {country['location']['lon']}, '{fd['type'][0]['name']}', '{fd['url']}', '{fd['name']}', '{overview_html}')")
+        except Exception as e:
+            print(e)
+
+
+    # write to file, join list with two extra lines
+    file.write('\n'.join(appendage))
+
+
+    # recalculate offset etc
+    current_offset += jumpsize
+    total_count = res['totalCount']
+
+
+
+# write file
+file.close()
