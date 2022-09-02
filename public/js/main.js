2ebe1a7d7b944614fe11907dd98cca85cde63720
larsaars <lurzapps@gmail.com>
2022-09-02 21:05:48 +0200

    wokring sidebar and mark extra on marker selection

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,14 @@
+// global bool if sidebar is open
+let sidebarIsClosed = true, selectedMarker = null;
+
 // detect if page is in mobile format or mobile phone
 // call every time newly because window can be resized
 function pageIsMobileFormat() {
     return ($(window).innerHeight() / $(window).innerWidth()) >= 1.4;
 }
+
+// do something with delay
+const delay = ms => new Promise(res => setTimeout(res, ms));
 
 function generateRandomColor() {
     return '#' + Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0');
@@ -46,7 +52,7 @@ function useBlack(baseColor) {
     return brightness > 150;
 }
 
-async function buildGeoJSON(files, colors) {
+async function buildGeoJSON(files, colors, isDisaster) {
     // the object to be returned
     let featureCollection = {
         type: 'FeatureCollection',
@@ -67,8 +73,29 @@ async function buildGeoJSON(files, colors) {
                 // add styling information (all elements from one event have the same color)
                 feature.properties = {};
                 feature.properties.fill = colors[eventIndex];
+                // add information about the event to the feature
+                feature.properties.eventIndex = eventIndex;
+                feature.properties.isDisaster = isDisaster;
+                // add the feature to the feature collection
                 featureCollection.features.push(feature);
             }
+        }
+    }
+
+    return featureCollection;
+}
+
+function getGeoJSONFromEvent(geoJSON, eventIndex) {
+    // the object to be returned
+    let featureCollection = {
+        type: 'FeatureCollection',
+        features: []
+    }
+
+    // retrieve all features from the geoJSON that have the eventIndex property
+    for (const feature of geoJSON.features) {
+        if (feature.properties.eventIndex === eventIndex) {
+            featureCollection.features.push(feature);
         }
     }
 
@@ -80,34 +107,92 @@ function getMarkerImage(event, useBlack) {
     return 'url(markers/' + color + '/' + event.toLowerCase().replace(' ', '_') + '.png)';
 }
 
-function openSideBar(text, color) {
+function openSideBar(map, marker, allGeoJson) {
+    // specific actions only take place if sidebar is not already opened
     // get color rgb values from hex
-    const rgb = hexToRgb(color);
+    const rgb = hexToRgb(marker.color);
 
-    // set sidebar text
-    $('#sidebarText').html(text);
     // open links in sidebar in new tab
     $('#sidebarText a').attr('target', '_blank');
 
-    // set sidebar opened
-    let sidebar = $('#sidebar');
+    // sidebar and content divs
+    const sidebar = $('#sidebar');
+    const sidebarContent = $('#sidebarContent');
 
     // sidebar width is different for mobile users (or mobile format)
     const sidebarWidth = pageIsMobileFormat() ? '100%' : '42%';
 
     // set margin left for main element, show sidebar, set color etc.
-    $('#main').css('marginLeft', sidebarWidth);
-    sidebar.css('width', sidebarWidth);
-    sidebar.css('background', `rgba(${rgb.r}, ${rgb.g}, ${rgb.b}, 0.12)`);
-    sidebar.show();
+    sidebarContent.css('background', `rgba(${rgb.r}, ${rgb.g}, ${rgb.b}, 0.22)`);
 
+    if (sidebarIsClosed) {
+        //$('#main').css('marginLeft', sidebarWidth);
+        sidebar.css('width', sidebarWidth);
+        sidebar.animate({
+            width: 'toggle'
+        }, {
+            complete: function () {
+                // set sidebar text
+                $('#sidebarText').html(marker.description);
+            }
+        });
+    } else {
+        // set sidebar text directly without animation
+        $('#sidebarText').html(marker.description);
+    }
 
+    // on the map, mark the region as selected (get event geo json and add it to the map as layer)
+    const eventGeoJSON = getGeoJSONFromEvent(allGeoJson, marker.eventIndex);
+
+    // remove marked layer and source if they already exist (sidebar was already opened for other event)
+    if (!sidebarIsClosed) {
+        map.removeLayer('marked-layer');
+        map.removeSource('marked');
+    }
+
+    // add source and layer
+    map.addSource('marked', {
+        'type': 'geojson',
+        'data': eventGeoJSON
+    });
+
+    // add news and disaster layer
+    map.addLayer(
+        {
+            'id': 'marked-layer',
+            'type': 'fill',
+            'source': 'marked',
+            'layout': {},
+            'paint': {
+                'fill-color': ['get', 'fill'],
+                'fill-opacity': 0.9
+            }
+        }
+
+    );
+
+    // set boolean to indicate sidebar is open and set selected marker
+    sidebarIsClosed = false;
+    selectedMarker = marker;
 }
 
 function closeSideBar() {
-    // set sidebar closed
-    $('#main').css('marginLeft', '0%');
-    $('#sidebar').hide();
-}
+    // do nothing if sidebar is already closed
+    if (sidebarIsClosed) {
+        return;
+    }
 
-const delay = ms => new Promise(res => setTimeout(res, ms));
+    // hide sidebar
+    $('#sidebar').animate({width: 'toggle'}, {
+        complete: function () {
+            // remove sidebar text on animation finish
+            $('#sidebarText').html('');
+        }
+    });
+    // remove marked layer and source
+    map.removeLayer('marked-layer');
+    map.removeSource('marked');
+    // set sidebar closed and selected marker null
+    sidebarIsClosed = true;
+    selectedMarker = null;
+}
