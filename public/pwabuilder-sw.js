df72b4db9244df71e1c3e15f79b7f52d9fc6f4ac
larsaars <lurzapps@gmail.com>
2022-09-15 13:43:57 +0200

    offline.html

--- public/pwabuilder-sw.js
+++ public/pwabuilder-sw.js
@@ -1,11 +1,10 @@
-// This is the "Offline page" service worker
+// This is the service worker with the combined offline experience (Offline page + Offline copy of pages)
+
+const CACHE = "pwabuilder-offline-page";
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
-const CACHE = "pwabuilder-page";
-
-// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
-const offlineFallbackPage = "ToDo-replace-this-name.html";
+const offlineFallbackPage = "offline.html";
 
 self.addEventListener("message", (event) => {
   if (event.data && event.data.type === "SKIP_WAITING") {
@@ -15,14 +14,21 @@ self.addEventListener("message", (event) => {
 
 self.addEventListener('install', async (event) => {
   event.waitUntil(
-    caches.open(CACHE)
-      .then((cache) => cache.add(offlineFallbackPage))
+      caches.open(CACHE)
+          .then((cache) => cache.add(offlineFallbackPage))
   );
 });
 
 if (workbox.navigationPreload.isSupported()) {
   workbox.navigationPreload.enable();
 }
+
+workbox.routing.registerRoute(
+    new RegExp('/*'),
+    new workbox.strategies.StaleWhileRevalidate({
+      cacheName: CACHE
+    })
+);
 
 self.addEventListener('fetch', (event) => {
   if (event.request.mode === 'navigate') {
