0ba303773a64565b680a12452abc55b1febdd63b
Lars <lars.lars.specht@gmail.com>
2023-02-21 16:26:58 +0100

    remove minification

--- gruntfile.js
+++ gruntfile.js
@@ -2,15 +2,15 @@ module.exports = function (grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         // define source files and their destinations
-        uglify: {
-            build: {
-                src: ['public/js/*.js', '!public/js/*.min.js'],  // source files mask
-                dest: 'public/js/',    // destination folder
-                expand: true,    // allow dynamic building
-                flatten: true,   // remove all unnecessary nesting
-                ext: '.min.js'   // replace .js to .min.js
-            }
-        },
+        // uglify: {
+        //     build: {
+        //         src: ['public/js/*.js', '!public/js/*.min.js'],  // source files mask
+        //         dest: 'public/js/',    // destination folder
+        //         expand: true,    // allow dynamic building
+        //         flatten: true,   // remove all unnecessary nesting
+        //         ext: '.min.js'   // replace .js to .min.js
+        //     }
+        // },
         cssmin: {
             target:{
                 files: [{
@@ -29,9 +29,10 @@ module.exports = function (grunt) {
 
     // load plugins
     grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
+    // grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-cssmin')
 
     // register tasks
-    grunt.registerTask('default', ['cssmin', 'uglify']);
+    // grunt.registerTask('default', ['cssmin', 'uglify']);
+    grunt.registerTask('default', ['cssmin']);
 };
